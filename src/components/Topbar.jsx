a24736772e59c57570bba4f6910e4de0df40863d
Leyla-Akkurt <leylaacinar@gmail.com>
2023-06-03 22:43:20 +0300

    topbar was completed

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,49 @@
+import "./topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft"> ZED Social</div>
+      <div className="topbarCenter">
+        <div className="topbarItem">
+
+
+         <SearchIcon className="searchIcon" />
+        <input className="topbarInput" type="search" placeholder="Search for friend, post or video"/>
+        </div>
+
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIcon">
+            <PersonIcon className="topbarIconItem"/>
+            <span className="topbarIconBadge">1</span>
+          </div>
+
+          <div className="topbarIcon">
+            <ChatIcon className="topbarIconItem"/>
+            <span className="topbarIconBadge">3</span>
+          </div>
+
+          <div className="topbarIcon">
+            <NotificationsIcon className="topbarIconItem"/>
+            <span className="topbarIconBadge">2</span>
+          </div>
+        </div>
+        <div>
+          <img src="src\assets\person\1.jpeg"  className="topbarImg"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import Topbar from '../../components/Topbar'
+import './home.css'
+
+function Home() {
+  return (
+    <div><Topbar/></div>
+  )
+}
+
+export default Home
